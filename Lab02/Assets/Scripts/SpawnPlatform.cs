a5c8b5846060e3f7d8ad0acffcf25a6dc989c2cf
Mateusz2042 <you@example.com>
2018-03-20 00:38:24 +0100

    Dodane laboratoria Part1

---
+++ Lab02/Assets/Scripts/SpawnPlatform.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnPlatform : MonoBehaviour
+{
+
+    // How long our level is.
+    public int maxPlatforms = 50;
+    // The actual game object
+    public GameObject platform;
+    public GameObject coin1;
+    public GameObject coin2;
+    public GameObject coin3;
+    // Range of positions where we will spawn a new platform
+    public float horizontalMin = 8f;
+    public float horizontalMax = 12f;
+    public float verticalMin = -3f;
+    public float verticalMax = 3f;
+    // Where our initial offset is.
+    private Vector2 originPosition;
+
+    // Use this for initialization
+    void Start()
+    {
+        originPosition = transform.position;
+        Spawn();
+    }
+
+    // Function that generates a new gameobject.
+    void Spawn()
+    {
+        int x = 0;
+        for (int i = 0; i < maxPlatforms; i++)
+        {
+            Vector2 randomPosition = originPosition + new Vector2(Random.Range(horizontalMin, horizontalMax), Random.Range(verticalMin, verticalMax));
+            Instantiate(platform, randomPosition, Quaternion.identity);
+            System.Random rng = new System.Random(System.DateTime.UtcNow.Millisecond);
+            x = rng.Next(12);
+            if (x < 4)
+                Instantiate(coin1, randomPosition + new Vector2(0.0f, 2.0f), Quaternion.identity);
+            if (x >= 4 && x < 8)
+                Instantiate(coin2, randomPosition + new Vector2(0.0f, 2.0f), Quaternion.identity);
+            if (x >= 8)
+                Instantiate(coin2, randomPosition + new Vector2(0.0f, 2.0f), Quaternion.identity);
+            originPosition = randomPosition;
+        }
+    }
+
+
+}
---
+++ Lab02/Assets/Scripts/pickupcoin.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class pickupcoin : MonoBehaviour
+{
+    void Start()
+    {
+        //SetCountText();
+    }
+    // For now just destroy the coin if the player runs into
+    // it. Could play a sound, add to a score or more here later!
+    //void OnTriggerEnter2D(Collider2D other)
+    //{
+    //    if (other.gameObject.CompareTag("Player"))
+    //    {
+    //        if (this.gameObject.tag == "coinSprite1")
+    //        {
+    //            //count+=1;
+    //            //Destroy(this.gameObject);
+    //            //SetCountText();
+    //            Debug.Log("Coin was picked up");
+    //        }
+            
+    //    }
+    //}
+
+    //public Text countText;
+    //public Text winText;
+    //public int count = 0;
+
+    //void SetCountText()
+    //{
+    //    countText.text = "Twój wynik: " + count.ToString();
+    //    if (count >= 9)
+    //    {
+    //        winText.text = "Wygrałeś!!!";
+    //    }
+    //}
+}
